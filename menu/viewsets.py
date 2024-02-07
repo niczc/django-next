ccba589f01993d3385a014c9e0602504c9e66002
nicolas <nicolas.leao99@gmail.com>
2024-02-07 14:55:23 -0300

    First commit

---
+++ menu/viewsets.py
@@ -0,0 +1,11 @@
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from .models import Menu
+
+from .serializers import MenuSerializer
+
+
+class MenuViewSet(viewsets.ModelViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    permission_classes = [AllowAny]
